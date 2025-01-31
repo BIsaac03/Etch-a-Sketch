6efcf283a7b439181f64abcc62ee84fa530d745e
Benjamin Isaac <benisaac03@gmail.com>
2025-01-30 22:56:53 -0600

    Add customizable grid sizes and associated button

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
-const body = document.querySelector("body")
+function createGrid(size){
+    const grid = document.querySelector(".grid")
+    grid.replaceChildren()
+    let sideLength = (800 / size) + "px" 
 
-
-for (let i = 0; i < 16; i++){
-    let row = document.createElement("div")
-    row.classList.add("row")
-    row.classList.add("row" + i)
+    for (let i = 0; i < size; i++){
+        let row = document.createElement("div")
+        row.classList.add("row")
+        row.classList.add("row" + i)
+        
+        for (let j = 0; j < size; j++){
+            let newDiv = document.createElement("div")
+            newDiv.classList.add("square")
+            newDiv.classList.add("row" + i)
+            newDiv.classList.add("column" + j)
+            let color = "rgb(" + (Math.random() * 255) + ", " + 
+                                (Math.random() * 255) + ", " + 
+                                (Math.random() * 255) + ")"
+            newDiv.style.backgroundColor = color;
+            newDiv.style.opacity = 0;
+            newDiv.style.height = sideLength;
+            newDiv.style.width = sideLength;
     
-    for (let j = 0; j < 16; j++){
-        let newDiv = document.createElement("div")
-        newDiv.classList.add("square")
-        newDiv.classList.add("row" + i)
-        newDiv.classList.add("column" + j)
-        let color = "rgb(" + (Math.random() * 255) + ", " + 
-                            (Math.random() * 255) + ", " + 
-                            (Math.random() * 255) + ")"
-        newDiv.style.backgroundColor = color;
-
-        //newDiv.addEventListener("mouseover", function (e) {
-
-        newDiv.addEventListener("mouseover", function (e) {
-            const style = window.getComputedStyle(newDiv);
-            let opacity = style.getPropertyValue("opacity");
-            newOpacity = parseFloat(opacity) + 0.1;
-            e.target.style.opacity = newOpacity;
-        });
-
-
-
-        row.appendChild(newDiv)
+            //newDiv.addEventListener("mouseover", function (e) {
+    
+            newDiv.addEventListener("mouseover", function (e) {
+                const style = window.getComputedStyle(newDiv);
+                let opacity = style.getPropertyValue("opacity");
+                newOpacity = parseFloat(opacity) + 0.1;
+                e.target.style.opacity = newOpacity;
+            });
+    
+            row.appendChild(newDiv)
+        }
+        grid.appendChild(row)
     }
-    body.appendChild(row)
 }
+
+const btn = document.querySelector("button")
+    btn.addEventListener("click", () => {
+        let size = prompt("How many cells do you want on each side of the grid?");
+        while (size > 100){
+            size = prompt("Wow, that's a lot of squares! Please choose a number no higher than 100.")
+        }
+        if (size < 2){
+            alert("... I mean, what did you expect?")
+        }
+        createGrid(size)
+    });
+
+    btn.addEventListener("mouseover", function (e) {
+        e.target.style.backgroundColor = "#ffcf95"
+    });
+
+    btn.addEventListener("mouseout", function (e) {
+        e.target.style.backgroundColor = "bisque";
+    });
+
+createGrid(16)
