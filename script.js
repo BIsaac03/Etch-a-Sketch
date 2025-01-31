cc04bbe1497389d2b4cdd3e7dee03e97ceb633c7
Benjamin Isaac <benisaac03@gmail.com>
2025-01-30 21:22:32 -0600

    Generate divs and gradually change color on click

---
+++ script.js
@@ -0,0 +1,30 @@
+const body = document.querySelector("body")
+
+
+for (let i = 0; i < 16; i++){
+    let row = document.createElement("div")
+    row.classList.add("row")
+    row.classList.add("row" + i)
+    
+    for (let j = 0; j < 16; j++){
+        let newDiv = document.createElement("div")
+        newDiv.classList.add("square")
+        newDiv.classList.add("row" + i)
+        newDiv.classList.add("column" + j)
+        newDiv.setAttribute("opacity", "0.0")
+
+        //newDiv.addEventListener("mouseover", function (e) {
+
+        newDiv.addEventListener("click", function (e) {
+            e.target.style.background = "blue";
+
+            const style = window.getComputedStyle(newDiv);
+            let opacity = style.getPropertyValue("opacity");
+            newOpacity = parseFloat(opacity) + 0.1;
+            e.target.style.opacity = newOpacity;
+        });
+
+        row.appendChild(newDiv)
+    }
+    body.appendChild(row)
+}
